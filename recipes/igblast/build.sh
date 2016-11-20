5607c0c7c42f905423b3d5c278288a7248a3c418
Marcel Martin <marcel.martin@scilifelab.se>
2016-11-20 21:50:21 +0100

    Bump IgBLAST to 1.6.1

--- recipes/igblast/build.sh
+++ recipes/igblast/build.sh
@@ -14,7 +14,7 @@ mkdir -p $PREFIX/bin
 mkdir -p $SHARE_DIR/bin
 
 if [ $(uname) == Linux ]; then
-    # Distributed IgBLAST 1.5.0 binaries set an RPATH that conda build complains build.
+    # The distributed IgBLAST binaries set an RPATH that conda build complains about
     for FILE in makeblastdb igblastn igblastp; do
         patchelf --remove-rpath bin/$FILE
     done
