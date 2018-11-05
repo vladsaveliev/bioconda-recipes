779d5f47c065a8eaff8e573f35632cafb3d03a36
vladsaveliev <vladislav.sav@gmail.com>
2018-11-05 14:43:02 +1100

    perl-bio-db-hts update and support macos

--- recipes/perl-bio-db-hts/build.sh
+++ recipes/perl-bio-db-hts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
+perl Build.PL --extra_compiler_flags "-I$PREFIX/include" --htslib "$PREFIX/include/htslib"
 perl ./Build
+perl ./Build test
 # Make sure this goes in site
 perl ./Build install --installdirs site
